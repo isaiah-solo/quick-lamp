26ddd3c89eea71bda456fe381bd65ccf95ffebd7
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2018-02-23 15:45:08 -0800

    documented provision.sh

--- provision.sh
+++ provision.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# Update headers
 apt-get update
+
+# Install tools
 apt-get install -y curl
+
+# Install Apache
 apt-get install -y apache2
+
+# Install PHP
 apt-get install -y php5 php5-curl php5-mysql php5-ssh2 php5-gd php5-imap php5-mcrypt
 
+# Install MySQL
 echo "mysql-server mysql-server/root_password password password" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password password" | debconf-set-selections
 apt-get install -y mysql-server
@@ -15,4 +23,8 @@ if ! [ -L /var/www/html ]; then
   ln -fs /vagrant/public /var/www/html
 fi
 
-service apache2 reload && chmod -R 755 /var/www/html/* && chown -R www-data:vagrant /var/www/html/*
+# Reload Apache
+service apache2 reload
+
+# Set proper permissions for web root
+chmod -R 755 /var/www/html/* && chown -R www-data:vagrant /var/www/html/*
