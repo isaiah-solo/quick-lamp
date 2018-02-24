e98ef254290815794a3155ca7116f682eae8dcdb
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2018-02-23 18:01:52 -0800

    added php-fpm to provision script

--- provision.sh
+++ provision.sh
@@ -4,13 +4,13 @@
 apt-get update
 
 # Install tools
-apt-get install -y curl
+apt-get install -y curl vim git
 
 # Install Apache
 apt-get install -y apache2
 
 # Install PHP
-apt-get install -y php5 php5-curl php5-mysql php5-ssh2 php5-gd php5-imap php5-mcrypt
+apt-get install -y php5 php5-fpm php5-curl php5-mysql php5-ssh2 php5-gd php5-imap php5-mcrypt
 
 # Install MySQL
 echo "mysql-server mysql-server/root_password password password" | debconf-set-selections
@@ -23,8 +23,9 @@ if ! [ -L /var/www/html ]; then
   ln -fs /vagrant/public /var/www/html
 fi
 
-# Reload Apache
+# Reload services
 service apache2 reload
+service php5-fpm reload
 
 # Set proper permissions for web root
 chmod -R 755 /var/www/html/* && chown -R www-data:vagrant /var/www/html/*
